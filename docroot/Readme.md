6bf00cc23ea929c4c819e102dca9fa8ad31fe725
Andrew Larcombe <andrew@andrewl.net>
2017-11-11 22:50:18 +0000

    First release.

---
+++ docroot/Readme.md
@@ -0,0 +1,28 @@
+Invaders from Space
+===================
+
+An Anti-Space Invaders
+----------------------
+
+You are one of forty friendly aliens trying to land on a planet to help them
+save their planet from ecological destruction.. During your descent though, the
+natives start firing at you. Ingrates! 
+
+Try to land any of your squadron of aliens on the alien planet. Move between
+your ships using the cursor keys. Your current ship is highlighted orange.
+Don't get shot!
+
+Why??!?!?!
+----------
+
+This was an unfinished entry for Ludum Dare 39. LD is a 48-hour game jam - make
+a game completely from scratch in 48 hours.
+
+Built with
+-----------
+
+Javascript using the phaser.io library.
+
+Music created with BeepBox - beepbox.co 
+Sprites were created using Piskel - piskelapp.com
+Sound FX created using Bfxr - bfxr.net
--- docroot/js/invaders_from_space.js
+++ docroot/js/invaders_from_space.js
@@ -1,30 +1,28 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update, render: render });
 
+// Preload our assets.
 function preload() {
 
-  //game.load.image('bullet', 'assets/games/invaders/bullet.png');
+  game.load.audio('game_music', ['./assets/invaders_from_space.ogg']);
+  game.load.audio('explosion', ['./assets/explosion.wav']);
+  game.load.audio('game_win', ['./assets/win.ogg']);
+  game.load.audio('game_lose', ['./assets/lose.ogg']);
   game.load.atlasJSONHash('bullet', './assets/bullet.png', './assets/bullet.json');
   game.load.atlasJSONHash('invader', './assets/alien.png', './assets/alien.json');
   game.load.atlasJSONHash('selected_invader', './assets/player.png', './assets/player.json');
-  game.load.atlasJSONHash('ship', './assets/player.png', './assets/player.json');
-  game.load.atlasJSONHash('kaboom', './assets/player.png', './assets/player.json');
-
-  game.load.audio('game_music', ['./assets/invaders_from_space.wav']);
-  //game.load.image('ship', 'assets/games/invaders/player.png');
-  //game.load.spritesheet('kaboom', 'assets/games/invaders/explode.png', 128, 128);
+  game.load.atlasJSONHash('shooter', './assets/shooter.png', './assets/shooter.json');
 
 }
 
-var player;
+// All these globals!
+var shooter;
 var aliens;
 var bullets;
 var bulletTime = 0;
 var cursors;
-var explosions;
 var score = 0;
 var scoreString = '';
 var scoreText;
-var lives;
 var firingTimer = 0;
 var stateText;
 var livingEnemies = [];
@@ -32,7 +30,14 @@ var player_position = -1;
 var new_player_position = -1;
 var playerTween;
 var music;
+var explosion;
+var win_sound;
+var lose_sound;
+var final_living_alien;
 
+/**
+ * Sets up the game
+ */
 function create() {
 
   game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -47,9 +52,9 @@ function create() {
   bullets.setAll('outOfBoundsKill', true);
   bullets.setAll('checkWorldBounds', true);
 
-  player = game.add.sprite(400, 500, 'ship');
-  player.anchor.setTo(0.5, 0.5);
-  game.physics.enable(player, Phaser.Physics.ARCADE);
+  shooter = game.add.sprite(400, 500, 'shooter');
+  shooter.anchor.setTo(0.5, 0.5);
+  game.physics.enable(shooter, Phaser.Physics.ARCADE);
 
   //  The baddies!
   aliens = game.add.group();
@@ -58,40 +63,30 @@ function create() {
 
   createAliens();
 
-  //  The score
+  // Display the score
   scoreString = 'Score : ';
-  scoreText = game.add.text(10, 10, scoreString + score, { font: '34px Arial', fill: '#fff' });
-
-  //  Lives
-  lives = game.add.group();
-  game.add.text(game.world.width - 100, 10, 'Lives : ', { font: '34px Arial', fill: '#fff' });
+  scoreText = game.add.text(10, 10, scoreString + score, { font: '34px Faster One', fill: '#ffffee' });
 
   //  Text
-  stateText = game.add.text(game.world.centerX,game.world.centerY,' ', { font: '84px Arial', fill: '#fff' });
+  stateText = game.add.text(game.world.centerX,game.world.centerY,' ', { font: '44px Faster One', fill: '#f00' });
   stateText.anchor.setTo(0.5, 0.5);
   stateText.visible = false;
 
-  for (var i = 0; i < 3; i++) 
-  {
-    var ship = lives.create(game.world.width - 100 + (30 * i), 60, 'ship');
-    ship.anchor.setTo(0.5, 0.5);
-    ship.angle = 90;
-    ship.alpha = 0.4;
-  }
-
-  //  An explosion pool
-  explosions = game.add.group();
-  explosions.createMultiple(30, 'kaboom');
-  explosions.forEach(setupInvader, this);
-
-  //  And some controls to play the game with
+  // This is what we control the player with
   cursors = game.input.keyboard.createCursorKeys();
 
+  // Create the sounds, start playing the music
+  explosion = new Phaser.Sound(game,'explosion',0.5,false);
   music = new Phaser.Sound(game,'game_music',0.75,true);
+  win_sound = new Phaser.Sound(game,'game_win',0.75,false);
+  lose_sound = new Phaser.Sound(game,'game_lose',0.75,false);
   music.play();
 
 }
 
+/**
+ * Creates the aliens, your buddies!
+ */
 function createAliens () {
 
   for (var y = 0; y < 4; y++)
@@ -107,6 +102,10 @@ function createAliens () {
     }
   }
 
+  // This is so we can keep tabs on the alien nearest the ground so that we
+  // can find out when we've landed.
+  final_living_alien = 39;
+
   aliens.x = 100;
   aliens.y = 50;
 
@@ -119,18 +118,21 @@ function createAliens () {
   //  When the tween loops it calls descend
   playerTween.onRepeat.add(descend, this);
 
-  movePlayer(player_position);
 
+  movePlayer(player_position);
 }
 
+  /**
 function setupInvader (invader) {
-
   invader.anchor.x = 0.5;
   invader.anchor.y = 0.5;
   invader.animations.add('kaboom');
-
 }
+*/
 
+/**
+ * Moves the aliens down one row, closer to the planet beneath them.
+ */
 function descend() {
   aliens.y += 10;
 
@@ -138,20 +140,23 @@ function descend() {
   score += 5;
   scoreText.text = scoreString + score;
 
-  if (aliens.y == 500) {
+  // This is our win condition!
+  if (aliens.y + aliens.children[final_living_alien].position.y > 500) {
     endGame(1);
   }
 }
 
 /**
- * The once per frame update
+ * The once per frame update.
  */
 function update() {
 
+  // If we've got a messsage onscreen then don't do anything.
   if (stateText.visible) {
     return;
   }
 
+  // Move the player according to the key pressed.
   if (cursors.left.isDown) {
     new_player_position = player_position - 1;
   }
@@ -172,20 +177,20 @@ function update() {
     }
   }
 
-  //  Reset the player, then check for movement keys
-  player.body.velocity.setTo(0, 0);
+  //  Reset the shooter, then check for movement keys
+  shooter.body.velocity.setTo(0, 0);
 
-  // Randomly move 4% of the time.
+  // Randomly move the shooter 4% of the time.
   moveIt = Math.random();
 
-  if (moveIt < 0.02 && player.position.x > 100) {
-    player.body.velocity.x = -1400;
+  if (moveIt < 0.02 && shooter.position.x > 100) {
+    shooter.body.velocity.x = -1400;
   }
-  else if (moveIt > 0.98 && player.position.x < 700) {
-    player.body.velocity.x = 1400;
+  else if (moveIt > 0.98 && shooter.position.x < 700) {
+    shooter.body.velocity.x = 1400;
   }
 
-  // Randomly fire?
+  // Randomly fire
   if (Math.random() < 0.25) {
     fireBullet();
   }
@@ -195,21 +200,26 @@ function update() {
 
 }
 
+/**
+ * The game has ended.
+ */
 function endGame(win = 0) {
 
   music.stop();
   playerTween.pause();
 
   if (win) {
+    win_sound.play();
     message = "You Won";
   }
   else {
+    lose_sound.play();
     message = "You Lost";
   }
+  message += "\nReload to restart";
 
   stateText.text = message;
   stateText.visible = true;
-
 
   //the "click to restart" handler
   game.input.onTap.addOnce(restart,this);
@@ -232,22 +242,34 @@ function movePlayer(new_position, old_position = -1) {
 
   if (old_position != -1) {
     aliens.children[old_position].loadTexture('invader');
+    aliens.children[old_position].play('fly');
   }
 
   return new_position;
 }
 
+/**
+ *  Handles the collision between sprites. End the game if the player has been hit.
+ */
 function collisionHandler (bullet, alien) {
 
-  console.log("collisionHandler");
-
   //  When a bullet hits an alien we kill them both
+  explosion.play();
   bullet.kill();
   alien.kill();
 
   if (alien.data.index == player_position) {
     endGame(0);
   }
+
+  if (alien.data.index == final_living_alien) {
+    for (i = 0; i < 39; i++) {
+      if (aliens.children[i].visible) {
+        final_living_alien = i;
+      }
+    }
+  }
+  console.log(final_living_alien);
 
 }
 
@@ -262,7 +284,7 @@ function fireBullet () {
     if (bullet)
     {
       //  And fire it
-      bullet.reset(player.x, player.y + 8);
+      bullet.reset(shooter.x, shooter.y + 8);
       bullet.body.velocity.y = -400;
       bulletTime = game.time.now + 800;
     }
@@ -271,28 +293,22 @@ function fireBullet () {
 }
 
 function resetBullet (bullet) {
-
   //  Called if the bullet goes out of the screen
   bullet.kill();
-
 }
 
 function restart () {
 
-  //  A new level starts
-
-  //resets the life count
-  lives.callAll('revive');
   //  And brings the aliens back from the dead :)
   aliens.removeAll();
   createAliens();
 
   //revives the player
   player.revive();
+
   //hides the text
   stateText.visible = false;
 
 }
 
-function render() {
-}
+function render() {}
